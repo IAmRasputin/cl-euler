5f763dea082acceae0a2b1ee39212a2bebba618a
Ryan Gannon <ryanmgannon.dev@gmail.com>
2025-05-26 17:44:40 -0400

    problem 5, now 20x faster!

--- solutions/problem-5.lisp
+++ solutions/problem-5.lisp
@@ -4,11 +4,10 @@
 
 (defun problem-5 ()
   ;; Not putting this one in the utils, too specific
-  (declare (optimize (speed 3) (safety 0) (space 0) (debug 0)))
   (labels ((divisible-by-all (n)
              (dotimes (count 18)
                (unless (zerop (mod n (+ 2 count)))
                  (return-from divisible-by-all nil)))
              t))
-    (do* ((index 1 (+ index 1)))
-         ((divisible-by-all index) index))))
+    (do* ((index 20 (+ index 20)))
+        ((divisible-by-all index) index))))
